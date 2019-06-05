1ad993a8a5a42f0a6ed7bcc693a2174e4cc24bf9
mmizani <maryam.mizani@gmail.com>
2019-06-05 12:10:25 -0400

    Server: Add a hoc comp for displaying axios errors in a Modal

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Auxiliary/Auxiliary';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {   //Anonymous class
+        state = {
+            error: null
+        }
+        componentDidMount () {
+            // set the error to null (default) before receiving the response
+            axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req; // without this, the req will not continue
+            });
+            axios.interceptors.response.use(response => response, 
+                // without returning the response, the res will not continue
+                error => {  
+                this.setState({error: error});
+                return 
+            });
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null});
+        }
+
+        render() {
+            return (
+                <Aux>
+                    <Modal 
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error ? this.state.error.message: null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+
+    }
+}
+export default withErrorHandler;
