67411cad45978bc3c1ad6967e7784203f0d881b7
mmizani <maryam.mizani@gmail.com>
2019-06-19 22:27:05 -0400

    FormValidation: Turn off the Order button if form is not valid

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,9 +84,11 @@ class ContactData extends Component {
                         { value: 'cheapest', displayValue: 'Cheapest' }
                     ]
                 },
-                value: ''
+                value: '',
+                valid: true
             }
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -145,7 +147,11 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        this.setState({orderForm : updatedOrderForm});
+        let formIsValid = true;
+        for(let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderForm : updatedOrderForm, formIsValid: formIsValid});
     }
 
     render() {
@@ -170,7 +176,7 @@ class ContactData extends Component {
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
                 <Button
-                    btnType="Success"
+                    btnType="Success" disabled={!this.state.formIsValid}
                     clicked={this.orderHandler}> ORDER</Button>
             </form>
         );
